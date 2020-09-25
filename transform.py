ff88bb69a31f9bcf5f6614d0436b2e3ed8cec4e1
Etienne Trimaille <gustrimaille@yahoo.fr>
2020-09-25 09:34:44 +0200

    Update 3Liz domain and add metadata for links

--- transform.py
+++ transform.py
@@ -8,6 +8,8 @@ import markdown
 from markdown.extensions.toc import TocExtension
 from markdown.extensions.fenced_code import FencedCodeExtension
 
+liz_domain = 'https://docs.3liz.org'
+
 mkin = open(sys.argv[1])
 md = markdown.Markdown(
     extensions=[
@@ -27,8 +29,20 @@ favicon = md_meta.get('favicon')
 if favicon:
     favicon = favicon[0]
 else:
-    favicon = 'https://3liz.github.io/favicon.png'
-link_sibling = md_meta.get('sibling')
+    favicon = '{}/favicon.png'.format(liz_domain)
+
+
+def parse_bool(text):
+    if isinstance(text, list):
+        text = text[0]
+    if text in ['True', True]:
+        return True
+    else:
+        return False
+
+
+link_parent_folder = parse_bool(md_meta.get('up'))
+link_current_folder = parse_bool(md_meta.get('index')) or parse_bool(md_meta.get('sibling'))
 
 output = list()
 output.append("""
@@ -38,7 +52,7 @@ output.append("""
   <meta charset="utf-8"/>
   <title>{title}</title>
   <link href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.1/styles/github.min.css" rel="stylesheet"/>
-  <link href="https://3liz.github.io/remarkable.css" rel="stylesheet"/>
+  <link href="{domain}/remarkable.css" rel="stylesheet"/>
   <link href="{favicon}" rel="icon" type="image/png" >
  </head>
  <body>
@@ -46,11 +60,13 @@ output.append("""
     <h1>{title}</h1>
  </header>
  <article>
- <p><a href="../">Up</a></p>
-""".format(title=doc_title, favicon=favicon))
+""".format(title=doc_title, favicon=favicon, domain=liz_domain))
 
-if link_sibling:
-    output.append("""<p><a href="./index.html">Back</a></p>""")
+if link_parent_folder:
+    output.append("""<p><a href="./../">Up</a></p>""")
+
+if link_current_folder:
+    output.append("""<p><a href="./">Index</a></p>""")
 
 regex_link = r'<a href="(.*).md">'
 result = re.sub(regex_link, r'<a href="\1.html">', gen_html)
